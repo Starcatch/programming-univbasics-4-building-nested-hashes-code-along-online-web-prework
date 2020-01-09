f28127fe9f079398720cdf9fad96dec872ec4045
Timur Mishiev <timurmishiev@gmail.com>
2020-01-09 00:55:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,7 +4,6 @@ def third_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {
-        #your key/value pairs here
         },
       :hero_friends => []
    }, 
